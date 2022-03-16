e84ca6ec6e40f6954ef13a2a3890f7f4179b748b
usamahzaman <99325810+usamahzaman@users.noreply.github.com>
2022-03-16 14:13:40 +0000

    Add files via upload
    VERSION 1 of webpage

---
+++ my_website V1/script/scripts.js
@@ -0,0 +1,9 @@
+function Para_Target()
+{
+    document.getElementById("target").innerHTML = "EVO TME SIX";
+}
+
+function Para_Supra()
+{
+    document.getElementById("Supra").innerHTML = "EVO NINE";
+}
